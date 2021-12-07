48480e3f3e38d798c85efdb52422d9a8a91b7b8f
José Martín <jmartincastro99@gmail.com>
2021-12-07 14:51:41 +0100

    Task3 init

---
+++ TFG/Assets/Scripts/CastlePart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastlePart : MonoBehaviour
+{
+
+    public Vector3 platform_position;
+    public GameObject requirement = null;
+
+    public bool attached;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attached = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

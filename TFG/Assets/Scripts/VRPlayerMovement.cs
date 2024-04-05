1cc011aae2289dcef2a4ebb70ffc980962a0e000
Svenchu <TheSoraHD@users.noreply.github.com>
2024-04-05 12:29:37 +0200

    Get hmdTransform directly in the VRPlayerMovement script

--- TFG/Assets/Scripts/VRPlayerMovement.cs
+++ TFG/Assets/Scripts/VRPlayerMovement.cs
@@ -6,13 +6,14 @@ using Valve.VR.InteractionSystem;
 
 public class VRPlayerMovement : MonoBehaviour
 {
-    public SteamVR_Action_Vector2 input;
     public float speed = 1.0f;
+    public SteamVR_Action_Vector2 input;
+    public Transform hmdTransform; //TO-DO: VR Fallback
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+        Vector3 direction = hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
         transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);
     }
 }

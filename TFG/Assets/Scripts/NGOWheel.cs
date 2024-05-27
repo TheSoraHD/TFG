feb440a23e84b9289d8612192df8fa572ca51255
Svenchu <TheSoraHD@users.noreply.github.com>
2024-05-27 11:36:05 +0200

    Improvements Drill/Wheel Task
    Logic + SFX

--- TFG/Assets/Scripts/NGOWheel.cs
+++ TFG/Assets/Scripts/NGOWheel.cs
@@ -9,19 +9,40 @@ using UnityEditor;
 public class NGOWheel : NetworkBehaviour
 {
     private NGOTagSnap m_snap;
-    public float drillTimer = 0.0f;
+    private float drillTimer = 0.0f;
+    private bool isInSlot;
+
     public bool snapped = false;
 
     private void Start()
     {
         m_snap = GetComponent<NGOTagSnap>();
+        isInSlot = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == m_snap.tagToSnap && drillTimer > 100.0f) {
-            m_snap.TagSnapRpc(collision.transform.position);
-            snapped = true;
+        if (!snapped) {
+            if (collision.gameObject.tag == "Drill" && isInSlot) {
+                drillTimer += Time.deltaTime;
+            }
+            if (collision.gameObject.tag == m_snap.tagToSnap) {
+                Debug.Log("Wheel inside the slot!");
+                isInSlot = true;
+                if (drillTimer > 100.0f) {
+                    m_snap.TagSnapRpc(collision.transform.position);
+                    snapped = true;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == m_snap.tagToSnap)
+        {
+            isInSlot = false;
+            Debug.Log("Wheel outside the slot!");
         }
     }
 }
